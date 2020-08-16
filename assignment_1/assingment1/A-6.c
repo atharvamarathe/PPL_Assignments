a722a842f53f8311320f1e9a8926c5eab8d7a2d1
Atharva Marathe <69299285+atharvamarathe@users.noreply.github.com>
2020-08-16 22:01:24 +0530

    Add files via upload

---
+++ assignment_1/assingment1/A-6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+	int j,i;
+	i=2;
+	while(i<101) {
+		j=2;
+		while(j<i) {
+			if(i%j==0) 
+				break;
+			else {
+				j=j+1;
+				}
+			}
+		if(i==j) {
+			printf("%d \n",i);
+		}
+		else {
+			i=i+1;
+		}
+	}
+	return 0;
+}
+
+
